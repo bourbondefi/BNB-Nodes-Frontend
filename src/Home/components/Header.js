64a268ad65b2facd1ff1e564ea8a8ae6eaa3f7f3
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2022-04-09 14:26:49 -0400

    remove buttons

--- src/Home/components/Header.js
+++ src/Home/components/Header.js
@@ -33,49 +33,8 @@ export default function Header() {
       <img src={logo} alt="" width={"100%"} style={{ marginTop: -48 }} />
       <Connect responsive={false} />
       <Typography variant="h6" marginTop={3}>
-        Lucky Cat, the highest stablecoin rewards in all of DeFi.
+        Lucky Cat, the highest rewards in all of DeFi.
       </Typography>
-      <ButtonContainer container>
-        <Grid item flexGrow={1} marginRight={1} marginTop={3}>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href='https://luckycat.money/Lucky_Cat_BUSD_Litepaper_202222.pdf';
-              }}
-              >
-                  Litepaper
-            </Button>
-        </Grid>
-        <Grid item flexGrow={1} marginRight={1} marginTop={3}>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href='https://pancakeswap.finance/swap?outputCurrency=0x6A253216Ee3D9c4B9e6Cbb68889A96b5F6433e05';
-              }}
-              >
-                  Buy on Pancakeswap
-            </Button>
-        </Grid>
-        <Grid item flexGrow={1} marginRight={1} marginTop={3}>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href='https://poocoin.app/tokens/0x6A253216Ee3D9c4B9e6Cbb68889A96b5F6433e05';
-              }}
-              >
-                  Chart
-            </Button>
-        </Grid>
-      </ButtonContainer>
     </Wrapper>
   );
 }
